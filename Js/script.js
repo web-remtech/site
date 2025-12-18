3278213908343e9d4d8028bda0053f69bb493459
remtech <remysenard1@gmail.com>
2025-12-19 00:43:46 +0100

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -33,3 +33,10 @@ contactForm.addEventListener('submit', (e) => {
 
     contactForm.reset();
 });
+
+const menuToggle = document.getElementById("menuToggle");
+const nav = document.querySelector(".nav");
+
+menuToggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
